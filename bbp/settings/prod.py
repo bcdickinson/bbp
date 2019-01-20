7d65d42aba0e41a478c0166900577e44a544d07b
Ben Dickinson <ben.dickinson@torchbox.com>
2019-01-20 11:13:00 +0000

    Use X-Forwarded-Proto header

--- bbp/settings/prod.py
+++ bbp/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *  # noqa
 DEBUG = False
 
 SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = 'X-Forwarded-Proto'
 
 SESSION_COOKIE_SECURE = True
-
 SESSION_COOKIE_HTTPONLY = True
