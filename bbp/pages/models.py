4ad80662f6def0e8b327a954b642f2deea126b43
Ben Dickinson <ben.dickinson@torchbox.com>
2019-04-02 22:18:22 +0100

    I need a hero

--- bbp/pages/models.py
+++ bbp/pages/models.py
@@ -1,18 +1,22 @@
-from wagtail.admin.edit_handlers import StreamFieldPanel
+from django.db import models
+
+from wagtail.admin.edit_handlers import StreamFieldPanel, MultiFieldPanel, FieldPanel
 from wagtail.core import blocks
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 from wagtail.embeds.blocks import EmbedBlock
 from wagtail.images.blocks import ImageChooserBlock
-
-from .blocks import HeroImageBlock
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.models import Image
 
 
 class BasicPage(Page):
     show_in_menus_default = True
 
+    hero_image = models.ForeignKey(Image, on_delete=models.SET_NULL, blank=True, null=True)
+    strapline = models.CharField(blank=True, max_length=64)
+
     body = StreamField([
-        ('hero_image', HeroImageBlock()),
         ('heading', blocks.CharBlock(classname="full title")),
         ('paragraph', blocks.RichTextBlock(
             features=['h2', 'h3', 'h4', 'bold', 'italic', 'ol', 'ul', 'hr', 'link', 'document-link'],
@@ -22,5 +26,9 @@ class BasicPage(Page):
     ])
 
     content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            ImageChooserPanel('hero_image'),
+            FieldPanel('strapline'),
+        ], heading="Hero image"),
         StreamFieldPanel('body'),
     ]
